6d2c2fe25e230109177573328930d88ed4eebb44
silver4life <abdallahanwarrrr@gmail.com>
2026-02-08 03:37:38 +0200

    edge_detector.v

--- edge_detector.v
+++ edge_detector.v
@@ -3,9 +3,11 @@
 
 
 module edge_detector(
-    input clk,reset,
+    input clk,
+    input reset,
     input signal, // signal to be edge detected
-    output positive_edge,negative_edge
+    output positive_edge,
+    output negative_edge
     );
     reg state;
 
